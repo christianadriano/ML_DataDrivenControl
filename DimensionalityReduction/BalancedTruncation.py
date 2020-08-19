4710bffcd7866f84cc21561cbcd0e347650d0f87
Christian Adriano, M.Sc., PMP <christian.adriano@gmail.com>
2020-08-19 23:45:25 +0200

    Create BalancedTruncation.py

---
+++ DimensionalityReduction/BalancedTruncation.py
@@ -0,0 +1,9 @@
+"""
+Run examples of balanced truncation, which is a dimensionality reduction technique
+
+Using https://python-control.readthedocs.io/en/0.8.3/index.html
+"""
+
+import control
+from control.matlab import *
+
